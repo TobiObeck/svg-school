951c4f819a8106c2c9ca066dc9a01a4ae262c966
Linus Behrbohm <linusbehrbohm@web.de>
2021-04-22 22:14:31 +0200

    Rename BASE_SIZE to VIEWBOX_SIZE

--- src/SVGGrid/SVGGrid.svelte
+++ src/SVGGrid/SVGGrid.svelte
@@ -4,7 +4,7 @@
 import range from 'lodash/range'
 
 const SUBSECTIONS = 10
-const BASE_SIZE = 100
+const VIEWBOX_SIZE = 100
 
 let containerElement
 
@@ -32,13 +32,13 @@ line
 </style>
 
 <div bind:this={containerElement}>
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 {BASE_SIZE} {BASE_SIZE}">            
-        {#each range(0, SUBSECTIONS + 1).map((value) => value * (BASE_SIZE / SUBSECTIONS)) as y}
-            <line x1="{0}" x2="{BASE_SIZE}" y1="{y}" y2="{y}"></line>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 {VIEWBOX_SIZE} {VIEWBOX_SIZE}">
+        {#each range(0, SUBSECTIONS + 1).map((value) => value * (VIEWBOX_SIZE / SUBSECTIONS)) as y}
+            <line x1="{0}" x2="{VIEWBOX_SIZE}" y1="{y}" y2="{y}"></line>
         {/each}
 
-        {#each range(0, SUBSECTIONS + 1).map((value) => value * (BASE_SIZE / SUBSECTIONS)) as x}
-            <line x1="{x}" x2="{x}" y1="{0}" y2="{BASE_SIZE}"></line>
+        {#each range(0, SUBSECTIONS + 1).map((value) => value * (VIEWBOX_SIZE / SUBSECTIONS)) as x}
+            <line x1="{x}" x2="{x}" y1="{0}" y2="{VIEWBOX_SIZE}"></line>
         {/each}
     </svg>
 </div>
--- src/SVGLayer/SVGLayer.svelte
+++ src/SVGLayer/SVGLayer.svelte
@@ -11,7 +11,7 @@ export const getSVGElement = () =>
     return svgElement
 }
 
-const BASE_SIZE = 100
+const VIEWBOX_SIZE = 100
 
 </script>
 
@@ -34,7 +34,7 @@ svg
 
 
 <div style="opacity: {opacity}">
-    <svg bind:this={svgElement} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 {BASE_SIZE} {BASE_SIZE}" >
+    <svg bind:this={svgElement} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 {VIEWBOX_SIZE} {VIEWBOX_SIZE}" >
         {@html svg}
     </svg>
 </div>
