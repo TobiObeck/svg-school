6b171bacfc6ced5f9716b6f862516f12d581c79d
Tobi Obeck <13554426+TobiObeck@users.noreply.github.com>
2021-04-22 21:56:34 +0200

    pushes first two levels

--- src/LEVELS.js
+++ src/LEVELS.js
@@ -1,19 +1,5 @@
-
-export default [
-    
-    {
-        heading: 'Introduction',
-        tutorialText: 'lorem...',
-        solutionSVG: '<rect x="0" y="0" width="300" height="100" fill="black" />' // '<circle cx="50" cy="50" r="40" stroke="black" stroke-width="3" fill="red" />'
-    }
-
-    /*
-    
-    */
-
-    // level 2
-    /*
-<circle cx="30" cy="30" r="10" />
+const level1 = `<rect x="20" y="20" width="60" height="60" fill="black" />`;
+const level2 = `<circle cx="30" cy="30" r="10" />
 <circle cx="50" cy="30" r="10" />
 <circle cx="70" cy="30" r="10" />
 
@@ -23,7 +9,12 @@ export default [
 
 <circle cx="30" cy="70" r="10" />
 <circle cx="50" cy="70" r="10" />
-<circle cx="70" cy="70" r="10" />
-    */
-    // ...
-]
+<circle cx="70" cy="70" r="10" />`;
+
+export default [
+  {
+    heading: "Introduction",
+    tutorialText: "lorem...",
+    solutionSVG: level2, // '<circle cx="50" cy="50" r="40" stroke="black" stroke-width="3" fill="red" />'
+  },
+];
